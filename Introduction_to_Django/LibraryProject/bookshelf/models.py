3e16829a24477b8f36b37d9e25f86f854b37199d
Elyas <elaman99619@gmail.com>
2025-08-27 14:32:16 +0300

    task 1 done updated

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -4,6 +4,6 @@ from django.db.models import CharField, IntegerField
 
 # Create your models here.
 class Book(models.Model):
-    title = CharField(max_length=200)
-    author = CharField(max_length=100)
-    publication_year = IntegerField()
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_year = models.IntegerField()
