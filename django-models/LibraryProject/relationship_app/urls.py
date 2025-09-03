121adbbd040d2a7896dc3c772759c7389d5289ec
Elyas <elaman99619@gmail.com>
2025-09-03 15:44:39 +0300

    task 2 updated

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import list_books, LibraryDetailView, home, signup_view
+from .views import list_books, LibraryDetailView, home, register
 
 urlpatterns = [
     path('', list_books, name='list-books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
-    path('register/', signup_view, name='register'),
+    path('register/', register, name='register'),
     path('home/', home, name='home'),
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout')
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -16,7 +16,7 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
 
-def signup_view(request):
+def register(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
