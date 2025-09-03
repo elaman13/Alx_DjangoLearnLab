23eef0e83d6b6fe6536f7aea15d452dad0625b77
Elyas <elaman99619@gmail.com>
2025-09-03 13:19:56 +0300

    task 2 completed

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,12 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, SignUpView, home, CustomLoginView, logout_view
 
 urlpatterns = [
     path('', list_books, name='list-books'),
-    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail')
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detail'),
+    path('signup/', SignUpView.as_view(template_name_field='relationship_app/library_detail.html'), name='signup'),
+    path('home/', home, name='home'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', logout_view)
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
 from django.views.generic.detail import DetailView
 from .models import Book
 from .models import Library
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def list_books(request):
@@ -12,3 +16,19 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy('home')
+    template_name = 'relationship_app/register.html'
+
+class CustomLoginView(LoginView):
+    template_name = "relationship_app/login.html"
+    redirect_field_name = True
+    success_url = reverse_lazy('home')
+
+def logout_view(request):
+    return render(request, 'relationship_app/logout.html')
+
+def home(request):
+    return render(request, 'relationship_app/home.html')
