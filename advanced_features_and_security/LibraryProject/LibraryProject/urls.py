0c07167005941b8fad7fcb0d08e2ffcb638f34d1
Elyas <elaman99619@gmail.com>
2025-09-11 17:05:33 +0300

    task 2 done

--- advanced_features_and_security/LibraryProject/LibraryProject/urls.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
+    path('shelf/', include('bookshelf.urls')),
     path('', include('django.contrib.auth.urls'))
 ]
--- advanced_features_and_security/LibraryProject/relationship_app/views.py
+++ advanced_features_and_security/LibraryProject/relationship_app/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 from django.shortcuts import render, redirect
 from .models import Library, Book
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
 from django.contrib.auth.decorators import user_passes_test
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -13,24 +14,33 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', context={'books': books})
 
+
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = "library"
 
+# Custom user creation form
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'email')
+
+# register page
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect(reverse_lazy('home'))
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
 
     return render(request, 'relationship_app/register.html', {'form': form})
 
 
+# home page
 def home(request):
     return render(request, 'relationship_app/home.html')
 
