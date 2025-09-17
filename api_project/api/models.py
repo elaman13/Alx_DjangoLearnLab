1d8b32771554122bb0603f61b8a9f6013f69920c
Elyas <elaman99619@gmail.com>
2025-09-17 10:56:25 +0300

    task 0 update

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=250)
-    autor = models.CharField(max_length=150)
+    author = models.CharField(max_length=150)
     
     def __str__(self):
         return self.title
