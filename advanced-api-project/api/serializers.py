c58e055e611bbafd87ef1deb59062be28f2c5f70
Elyas <elaman99619@gmail.com>
2025-09-25 13:07:43 +0300

    added read_only to the serializer of Author model

--- advanced-api-project/api/serializers.py
+++ advanced-api-project/api/serializers.py
@@ -14,7 +14,7 @@ class BookSerializer(serializers.ModelSerializer):
 
 
 class AuthorSerializer(serializers.ModelSerializer):
-    book = BookSerializer(many=True, readonly=True)
+    book = BookSerializer(many=True, read_only=True)
     
     class Meta:
         model = Author
