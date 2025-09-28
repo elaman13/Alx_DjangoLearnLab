27f451ea3380286e71cc0b370e45e5c842f22aff
Elyas <elaman99619@gmail.com>
2025-09-28 13:31:06 +0300

    task 2 update

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -10,11 +10,13 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
-    filter_backends = [rest_framework.DjangoFilterBackend, filters.OrderingFilter]
+    filter_backends = [rest_framework.DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
     filterset_fields = ['title', 'author', 'publication_year']
     
     ordering_fields = ['title', 'publication_year']
     ordering = ['title']
+    
+    search_fields = ['title', 'publication_year']
     
     permission_classes = [IsAuthenticatedOrReadOnly]
 
