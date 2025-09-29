a99e224b122d3e73988235e636f4c901f4d901af
Elyas <elaman99619@gmail.com>
2025-09-29 20:13:37 +0300

    task 1: fix static files directory errors

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+
+
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE) # Foreign key
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE) # Foreign key
     published_date = models.DateTimeField(auto_now_add=True)
-    
-    
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,53 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from . import forms
+from django.http import HttpResponse
 
 # Create your views here.
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    
+    return render(request, 'blog/login.html', {'form': form})
+
+def register_view(request):
+    if request.method == 'POST':
+        form = forms.CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = forms.CustomUserCreationForm()
+    
+    return render(request, 'blog/register.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def home_view(request):
+    return render(request, 'blog/home.html')
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        new_username = request.POST.get('username')
+        new_email = request.POST.get('email')
+        
+        user = request.user
+        user.username= new_username
+        user.email = new_email
+        user.save()
+        
+        return redirect('profile')
+        
+    else:
+        return render(request, 'blog/profile.html')
