be56d490233c99c24a3d364d99140206c8966015
Elyas <elaman99619@gmail.com>
2025-10-01 14:24:26 +0300

    task 3 (update): update form model to Comment

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from . import models
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from .models import Comment
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -17,5 +18,5 @@ class PostForm(forms.ModelForm):
 
 class CommentForm(forms.ModelForm):
     class Meta:
-        model = models.Comment
+        model = Comment
         fields = ['content']
