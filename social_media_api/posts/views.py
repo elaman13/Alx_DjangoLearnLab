8677217dcd1d37ece6892c1b52b00cb2e131614a
Elyas <elaman99619@gmail.com>
2025-10-06 15:53:48 +0300

    task 1: update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,15 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework import viewsets
 from rest_framework import permissions
 from . import models
 from . import serializers
 
 # Create your views here.
-class PostViewSet(ModelViewSet):
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = models.Post.objects.all()
     serializer_class = serializers.PostSerializer
     permission_classes = [permissions.IsAuthenticated]
-    
-    def get_queryset(self):
-        user = self.request.user
-        return models.Post.objects.filter(id=user.id)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = models.Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+    permission_classes = [permissions.IsAuthenticated]
