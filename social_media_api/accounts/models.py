e48eb48317dc8662dd70ded2f8ff1e9bc1c5b55b
Elyas <elaman99619@gmail.com>
2025-10-06 12:04:29 +0300

    week 15: step 0 start

---
+++ social_media_api/accounts/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.CharField(max_length=250)
+    profile_picture = models.ImageField()
+    followers = models.ManyToManyField('self', symmetrical=False)
