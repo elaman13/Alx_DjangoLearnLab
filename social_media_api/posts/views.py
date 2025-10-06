42915e990395ecf3b4f4f09dc99b62ee8c51852b
Elyas <elaman99619@gmail.com>
2025-10-06 15:51:30 +0300

    task 1: update

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+from . import models
+from . import serializers
 
 # Create your views here.
+class PostViewSet(ModelViewSet):
+    serializer_class = serializers.PostSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_queryset(self):
+        user = self.request.user
+        return models.Post.objects.filter(id=user.id)
