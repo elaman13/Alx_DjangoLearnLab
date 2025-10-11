130f38d9e3d41a2af0fe8778c9522095166167b7
Elyas <elaman99620@gmail.com>
2025-10-11 14:02:23 +0300

    task 4: update

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,10 +85,15 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv("dbname"),      
+        'USER': os.getenv("user"),        
+        'PASSWORD': os.getenv("password"),
+        'HOST': os.getenv("host"),        
+        'PORT': os.getenv("port"),        
     }
 }
+
 
 
 # Password validation
